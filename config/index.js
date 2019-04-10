3da748479ee0169d876fefe09ee719012ce4fc10
陳陳 <a26933712@gmail.com>
2019-04-10 14:15:34 +0800

    update

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/home/',
+    assetsPublicPath: '/vue-shop/dist/',
     proxyTable: {},
 
     // Various Dev Server settings
